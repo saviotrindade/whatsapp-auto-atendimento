138fc37e37b2a07cc073178a074ac050cfabeab7
Sávio Bandeira <band.savio@gmail.com>
2024-06-16 16:31:10 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    puppeteer: {
+        headless: false
+    }
+});
+
+client.on('qr', (qr) =>{
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () =>{
+    console.log('Client is ready!');
+});
+
+client.on('message', (msg) =>{
+    if ( msg.body == '!ping' ) {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
